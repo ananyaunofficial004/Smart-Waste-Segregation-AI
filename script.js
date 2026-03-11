23831a9a4680d497a17d8cbc2b61a1e835af0982
ananyaunofficial004 <ananya.unofficial.004@gmail.com>
2026-03-11 20:00:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let score = 0;
 let detectedAlready = false;
 
+
 function detectWaste(){
 
 const input = document.getElementById("imageUpload");
@@ -10,35 +11,38 @@ document.getElementById("result").innerHTML="Please upload an image";
 return;
 }
 
+if(detectedAlready){
+document.getElementById("result").innerHTML="Upload a new image to score again";
+return;
+}
+
 const fileName = input.files[0].name.toLowerCase();
 
 let detected = "";
 
 if(fileName.includes("plastic")){
 detected = "Plastic → Blue Recycling Bin";
-score += 10;
 }
 
 else if(fileName.includes("banana") || fileName.includes("food")){
 detected = "Organic → Green Bin";
-score += 10;
 }
 
 else if(fileName.includes("paper")){
 detected = "Paper → White Bin";
-score += 10;
 }
 
 else{
 detected = "Metal → Yellow Bin";
-score += 10;
 }
+
+score += 10;
 
 document.getElementById("score").innerHTML = score;
 
 document.getElementById("result").innerHTML =
 "Detected Waste: " + detected;
 
-input.value = "";   // ⭐ important fix
+detectedAlready = true;
 
 }
